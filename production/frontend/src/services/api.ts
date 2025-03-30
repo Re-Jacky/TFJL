35ec3d741c57e47554d04be91f892589c92f6827
unknown <zyao1128@gmail.com>
2025-03-30 22:29:40 +0800

    refactor file structure

--- production/frontend/src/services/api.ts
+++ production/frontend/src/services/api.ts
@@ -1,24 +1,27 @@
 import { pid } from "process";
 import proxy from "./proxy";
 import type { Wnd } from "../types";
+
+type ScriptType = 'collab' | 'activity';
+
 export interface API {
-    getFileList: () => Promise<{ files: string[] }>;
-    readFile: (file: string) => Promise<string>;
+    getFileList: (type: ScriptType) => Promise<{ files: string[] }>;
+    readFile: (file: string, type: ScriptType) => Promise<string>;
     healthCheck: () => Promise<{ status: string }>;
     getWindows: () => Promise<{ windows: Array<Wnd>}>;
     startAction: (config: {pid: number; action: string;}) => Promise<{ status: string }>;
-    saveFile: (file: string, content: string) => Promise<{ status: string }>;
-    deleteFile: (file: string) => Promise<{ status: string }>;
+    saveFile: (file: string, content: string, type: ScriptType) => Promise<{ status: string }>;
+    deleteFile: (file: string, type: ScriptType) => Promise<{ status: string }>;
 }
 
 export const api: API = {
-    getFileList: async () => {
-        return await proxy.get('get-file-list');
+    getFileList: async (type: 'collab' | 'activity') => {
+        return await proxy.get(`get-file-list?type=${type}`);
     },
-    readFile: async (file: string) => {
+    readFile: async (file: string, type: ScriptType) => {
         // encode file name
         file = encodeURIComponent(file);
-        return await proxy.post('read-file', { file });
+        return await proxy.post('read-file', { file, type });
     },
     healthCheck: async () => {
         return await proxy.get('health');
@@ -29,10 +32,10 @@ export const api: API = {
     startAction: async (config) => {
         return await proxy.post('start-action', config);
     },
-    saveFile: async (file: string, content: string) => {
-        return await proxy.post('save-file', { file, content });
+    saveFile: async (file: string, content: string, type: ScriptType) => {
+        return await proxy.post('save-file', { file, content, type });
     },
-    deleteFile: async (file: string) => {
-        return await proxy.post('delete-file', { file });
+    deleteFile: async (file: string, type: ScriptType) => {
+        return await proxy.post('delete-file', { file, type });
     },
 }
