8a96ddd14ce4ac9944308b8785e43fcbb1fcea7e
unknown <zyao1128@gmail.com>
2026-04-06 11:33:35 +0800

    auto scroll the logs to the latest

--- production/frontend/src/components/header/LogMonitor.tsx
+++ production/frontend/src/components/header/LogMonitor.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import { Card, List } from 'antd';
 import styles from './LogMonitor.module.scss';
 import { useAppSelector } from '@src/store/store';
@@ -6,10 +6,18 @@ import { selectLogRecords } from '@src/store/selectors';
 
 const LogMonitor: React.FC = () => {
   const logRecords = useAppSelector(selectLogRecords)
+  const listRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    if (listRef.current) {
+      listRef.current.scrollTop = listRef.current.scrollHeight;
+    }
+  }, [logRecords]);
 
   return (
     <Card className={styles.logMonitor}>
       <List
+        ref={listRef}
         dataSource={logRecords}
         renderItem={(log) => (
           <List.Item className={styles.listItem}>
